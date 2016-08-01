715ec8ea0c8dad4327e7429fe88ce40d383178c2
javierspht <javierspht@gmail.com>
2016-08-01 15:24:27 +0200

    unitymeodia
    error1

---
+++ Concilio1/Assets/colibri/script comentado/ChangeColors.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Rhythmify
+{
+
+	public class ChangeColors : _AbstractRhythmObject
+	{
+		public GameObject prefab;
+//objeto a instanciar
+		public Vector3[] posiciones;
+//posiciones en las que instanciar
+		public int[] indices1;
+//indices para decidir
+		public int offset;
+//beat de comienzo
+
+		override protected void beatUpdate (int beat)
+		{
+			int size = posiciones.Length;
+
+			if (size < 0) {
+				return;
+			}
+			if (beat >= offset) {// en total hasta aquí es si hay posiciones en el array y ha comenzado la canción
+				int idx = beat - offset;
+				if (indices1.Length > 0) {
+					idx = indices1 [idx % indices1.Length];//cogemos el indice que toque
+				}
+
+				if (idx < 0) {
+					return;
+				}
+				//si el indice no es menor que cero 
+				Vector3 t = posiciones [idx % size];
+				Instantiate (prefab, t, transform.rotation);//instanciamos un objeto en la posición indicada
+			}
+		}
+	}
+}
---
+++ Concilio1/Assets/colibri/script comentado/MusicWrapper.cs
@@ -0,0 +1,25 @@
+﻿/* @Author: Gkxd
+ * 
+ * A wrapper for attaching information to Audio Sources and controlling music loops
+ * 
+ * */
+using UnityEngine;
+using System.Collections;
+
+namespace Rhythmify {
+    public class MusicWrapper : MonoBehaviour {
+        public int BPM;
+        private AudioSource audioSource;
+        private AudioClip audioClip;
+    
+        public void Start() {
+            if (gameObject.tag != "Rhythmify_Music") {
+                Debug.LogError("GameObjects with a MusicWrapper component must be tagged \"Rhythmify_Music\".");
+                Debug.Break();
+            }
+
+            audioSource = gameObject.GetComponent<AudioSource>();
+            audioClip = audioSource.clip;
+        }
+    }
+}
