0e70dd1b3d5aa74fb08cd3f25d92fa08b6a12183
javierspht <javierspht@gmail.com>
2016-08-05 01:38:26 +0200

    DeleteObjectDone
    Proyecto después de realizar la eliminación de objetos al llegar a un
    punto

---
+++ Concilio1/Assets/colibri/script comentado/DeleteObjects.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeleteObjects : MonoBehaviour {
+
+	// Use this for initialization
+//	void Start () {
+//		collision = new Collision ();
+//	
+//	}
+	
+	// Update is called once per frame
+//	void Update () {
+//		OnCollisionEnter (collision);
+
+//	}
+
+	public void OnTriggerEnter(Collider collision) 
+	{
+		Destroy (collision.gameObject);
+	}
+}
