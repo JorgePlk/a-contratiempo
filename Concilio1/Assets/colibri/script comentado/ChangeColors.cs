871dfb7b39521c5e3a8499d9d1cb8efb46e3fd79
javierspht <javierspht@gmail.com>
2016-09-03 19:59:09 +0200

    IndianaSongPart3
    Tercera parte de la realizacion de la cancion de indiana jones

--- Concilio1/Assets/colibri/script comentado/ChangeColors.cs
+++ Concilio1/Assets/colibri/script comentado/ChangeColors.cs
@@ -16,7 +16,7 @@ namespace Rhythmify
 
 		override protected void beatUpdate (int beat)
 		{
-			if (beat == bpm [indice]) {
+			if (beat >= bpm [indice]) {
 				Vector3 v = zonas [donde [indice]].transform.position + new Vector3 (0, 0, 0);
 				Instantiate(elementos[elto[indice]],v,zonas[donde[indice]].transform.rotation);
 				indice ++;
